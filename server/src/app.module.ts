b425aa42699fb10389da8615ee0668316f1f1f2d
ArceDaniel <jonathandanielarce9@gmail.com>
2023-03-28 13:50:11 -0300

    asd

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,6 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
+
+
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
