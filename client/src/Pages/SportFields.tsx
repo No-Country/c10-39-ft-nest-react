2e0970c4c362a752836fa4ee4caccf73914dc5cd
Felipe Suarez <felipesuarez1525@gmail.com>
2023-03-30 17:14:25 -0300

    add sportFields-page width mock

--- client/src/Pages/SportFields.tsx
+++ client/src/Pages/SportFields.tsx
@@ -1,7 +1,19 @@
 import { FC } from "react";
+import Layout from "../Components/Layout";
+import SportField from "../Components/SportField";
 
 const SportFields: FC = () => {
-  return <div></div>;
+  const Data = ["1", "1", "1", "1", "1", "1", "1"];
+
+  return (
+    <Layout title="Canchas">
+      <div className="flex flex-col gap-5 my-5">
+        {Data.map((item, index) => (
+          <SportField key={index} />
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default SportFields;
