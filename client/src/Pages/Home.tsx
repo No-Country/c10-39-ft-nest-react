12b7484c41dd76b48fa1b448c36970ab23bdff39
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-02 18:40:42 -0300

    fix boludes

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -3,10 +3,8 @@ import Card from "../Components/Card";
 const Home = () => {
   return (
     <Layout title="Home">
-      <Card title="Reservas">
-        <div className="h-[185px] bg-cover w-full bg-[url('https://www.rere.jp/beginners/uploads/2019/09/i-471621500-3-1024x667.jpg')]"></div>
-      </Card>
-      <Card title="Propietarios"></Card>
+      <Card title="Reservas" />
+      <Card title="Propietarios" />
     </Layout>
   );
 };
