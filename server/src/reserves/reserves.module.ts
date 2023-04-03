6dc37378c78d2e12144954ca2c65765551ba794a
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-03 16:08:59 -0300

    a

--- server/src/reserves/reserves.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReservesService } from './reserves.service';
-import { ReservesController } from './reserves.controller';
-
-@Module({
-  controllers: [ReservesController],
-  providers: [ReservesService]
-})
-export class ReservesModule {}
