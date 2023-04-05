bed96738f975b5e161fc25e3b6bd25fea1c74469
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-05 20:29:23 -0300

    add profile page

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -1,10 +1,37 @@
-import { FC } from "react";
-import Layout from "../Components/Layout";
+import { FC, useState } from 'react';
+import Layout from '../Components/Layout';
+import Input from '../Components/Input';
+import { MdEdit } from 'react-icons/md';
 
 const Profile: FC = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Layout title="Perfil">
-      <div></div>
+      <div className="flex flex-col items-center w-full">
+        <div className="bg-[#000] w-32 h-32 rounded-3xl m-10"></div>
+        <div className="w-full flex flex-col items-center gap-5">
+          <Input type="text" label="Nombre" state={name} setState={setName} icon={<MdEdit />} />
+          <Input
+            type="text"
+            label="Apellido"
+            state={lastName}
+            setState={setLastName}
+            icon={<MdEdit />}
+          />
+          <Input type="mail" label="Mail" state={mail} setState={setMail} icon={<MdEdit />} />
+          <Input
+            type="password"
+            label="Contraseña"
+            state={password}
+            setState={setPassword}
+            icon={<MdEdit />}
+          />
+        </div>
+      </div>
     </Layout>
   );
 };
