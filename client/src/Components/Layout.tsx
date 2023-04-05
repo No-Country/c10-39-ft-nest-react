ba405011ad01b11a90e56791e46ddf3df92422f2
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-05 00:20:02 -0300

    add dinamyc close menus in layout, update box sizes in menus

--- client/src/Components/Layout.tsx
+++ client/src/Components/Layout.tsx
@@ -16,36 +16,67 @@ import { Link } from "react-router-dom";
 const Layout: FC<props> = ({ children, title }) => {
   const [openMenu, setOpenMenu] = useState(false);
   const [openSecondMenu, setOpenSecondMenu] = useState(false);
+  const [openSportMenu, setOpenSportMenu] = useState(false);
 
-  const handleClickMenu = () => setOpenMenu(!openMenu);
+  const handleClickMenu = () => {
+    setOpenMenu(!openMenu);
+    setOpenSecondMenu(false);
+    setOpenSportMenu(false);
+  };
   const handleCloseMenu = () => setOpenMenu(false);
-  const handleClickSecondMenu = () => setOpenSecondMenu(!openSecondMenu);
+  const handleClickSecondMenu = () => {
+    setOpenSecondMenu(!openSecondMenu);
+    setOpenMenu(false);
+    setOpenSportMenu(false);
+  };
+  const handleClickSportsMenu = () => {
+    setOpenSportMenu(!openSportMenu);
+    setOpenMenu(false);
+    setOpenSecondMenu(false);
+  };
 
   return (
     <div>
       <header>
-        <nav className="z-[300] px-5 bg-primary flex justify-center shadow-lg fixed w-full h-[80px]">
-          <div className=" gap-7 justify-start flex w-full items-center pb-5 pt-10 ">
-            <button className="text-white text-2xl align-middle" onClick={handleClickMenu}>
+        <nav className="z-[500] px-5 bg-primary flex justify-center shadow-lg fixed w-full h-[80px]">
+          <div className=" gap-7 justify-start flex w-full items-center pb-5 pt-10">
+            <button className="text-white text-2xl" onClick={handleClickMenu}>
               <GiHamburgerMenu />
             </button>
-            <h1 className="text-white text-2xl w-10/12 text-left font-semibold">{title}</h1>
+            <h1 className="text-white text-xl w-full pr-1 text-left font-semibold">{title}</h1>
           </div>
-          <div className=" gap-7 justify-end flex w-full items-center pb-5 pt-10 ">
-            <button className="text-white align-middle text-2xl -rotate-90">
+          <div className="relative gap-7 justify-end flex items-center pb-5 pt-10 ">
+            <button
+              className={`${
+                openSportMenu ? "rotate-90" : "-rotate-90"
+              } text-white align-middle text-2xl transition-transform`}
+              onClick={handleClickSportsMenu}
+            >
               <MdKeyboardArrowLeft />
             </button>
-            <div className="relative text-white text-2xl">
+            <ul
+              className={`${
+                openSportMenu ? "flex" : "hidden"
+              } text-white text-2xl flex-col justify-around absolute -left-56 top-10 w-52 bg-[#000] py-10 rounded-md`}
+            >
+              <li className="pl-5 py-5 active:bg-primary">
+                <Link to={"/reservas/tenis"}>Tenis</Link>
+              </li>
+              <li className="pl-5 py-5 active:bg-primary">
+                <Link to={"/reservas/futbol"}>Futbol</Link>
+              </li>
+            </ul>
+            <div className="text-white text-2xl">
               <div onClick={handleClickSecondMenu}>
                 <GoKebabVertical />
               </div>
-              <div
+              <ul
                 className={`${
                   openSecondMenu ? "flex" : "hidden"
-                }   items-center absolute -left-52 top-0 h-40 w-52 bg-[#000] py-5 rounded-md`}
+                }   items-center absolute -left-40 top-10 h-40 w-52 bg-[#000] py-10 rounded-md`}
               >
-                <span className="pl-5 py-5 active:bg-primary w-full">Cerrar sesion</span>
-              </div>
+                <li className="pl-5 py-5 active:bg-primary w-full">Cerrar sesion</li>
+              </ul>
             </div>
           </div>
           <div
@@ -54,7 +85,7 @@ const Layout: FC<props> = ({ children, title }) => {
             } transition-transform absolute left-0 w-5/6 h-screen bg-[#000] text-white pt-10`}
           >
             <div className="flex flex-row w-full justify-between px-5">
-              <Link to={"/home"} className="text-2xl">
+              <Link onClick={handleCloseMenu} to={"/home"} className="text-2xl">
                 <FaBasketballBall />
               </Link>
               <div className="text-2xl" onClick={handleClickMenu}>
