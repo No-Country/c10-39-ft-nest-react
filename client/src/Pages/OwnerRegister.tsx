1a1d1e2aa4b48bedfddf245459ac10cd5359a041
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-05 16:18:17 -0600

    feat: view owner register

--- client/src/Pages/OwnerRegister.tsx
+++ client/src/Pages/OwnerRegister.tsx
@@ -1,10 +1,22 @@
-import { FC } from "react";
-import Layout from "../Components/Layout";
+import { FC } from 'react';
+import Layout from '../Components/Layout';
+import PrimaryButton from '../Components/PrimaryButton';
+import Input from '../Components/Input';
 
 const OwnerRegister: FC = () => {
   return (
-    <Layout title="Registrarse como propietario">
-      <div></div>
+    <Layout title="Propietario">
+      <h2 className="text-center text-3xl mt-[50px]">Registrarse</h2>
+      <div className="bg-[#D9D9D9] cursor-pointer mx-1 my-[70px] relative rounded h-[227px] text-center ">
+        +
+      </div>
+      <div className="flex flex-col w-full items-center gap-10">
+        <Input type="text" label="Documento" state="" setState={() => {}} />
+        <Input type="text" label="Numero de telefono" state="" setState={() => {}} />
+      </div>
+      <div className="absolute bottom-10 right-10">
+        <PrimaryButton text="BUSCAR" />
+      </div>
     </Layout>
   );
 };
