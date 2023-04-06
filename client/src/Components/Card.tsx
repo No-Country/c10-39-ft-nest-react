a520fb76bae60f6657ee5931761e69415cb1cf4a
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-06 17:37:53 -0300

    add navbars responsive components

--- client/src/Components/Card.tsx
+++ client/src/Components/Card.tsx
@@ -13,14 +13,14 @@ const Card: FC<props> = ({ title, route }) => {
 
   const handleClick = () => navigate(`${route}`);
   return (
-    <div className="w-full">
+    <div className="w-full lg:max-w-[700px]">
       <div className="relative rounded-t-lg bg-primary bg-opacity-80 w-full h-[150px] lg:h-64">
-        <h2 className=" absolute bottom-4 left-4 text-white text-xl w-10/12 text-left font-semibold lg:text-3xl">
+        <h2 className=" absolute bottom-4 left-10 text-white text-xl w-10/12 text-left font-semibold lg:text-3xl">
           {title}
         </h2>
       </div>
       <div className=" relative rounded-b-lg w-full h-[48px] shadow-lg bottom-0 bg-white">
-        <div className="absolute bottom-5 right-1">
+        <div className="absolute bottom-5 right-10">
           <PrimaryButton text="IR" onClick={handleClick} />
         </div>
       </div>
--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <Layout title="Home">
       <div className="w-full h-full overflow-scroll fixed bg-tenis bg-cover bg-[45%]">
-        <div className="flex flex-col gap-16 mx-1 h-full pt-24 relative lg:flex-row lg:mx-20 lg:pt-0 lg:items-center lg:bottom-20">
+        <div className="flex flex-col gap-16 mx-1 h-full pt-24 relative lg:flex-row lg:mx-20 lg:pt-0 lg:items-center lg:bottom-20 lg:justify-between">
           <Card route="/reservas" title="Reservas" />
           <Card route="/propietarios" title="Propietarios" />
         </div>
