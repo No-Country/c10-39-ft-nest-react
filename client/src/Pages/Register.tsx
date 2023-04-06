9de5cd406ffb3e45b761d67a4d89ccbe5d6b069d
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-06 14:04:42 -0600

    fix: register desktop view

--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -43,45 +43,47 @@ const Register: FC = () => {
   };
 
   return (
-    <Layout title='Registrarse'>
-      <form onSubmit={handleSubmit} className='flex flex-col w-full items-center'>
-        <div className='flex flex-col w-full items-center gap-5 py-12'>
-          <Input type='mail' label='Mail' state={email} setState={setEmail} icon={<IoMdMail />} />
-          <Input
-            type='text'
-            label='Nombre'
-            state={name}
-            setState={setName}
-            icon={<HiOutlineUser />}
+    <Layout title="Registrarse">
+      <div className=" w-full m-auto lg:w-2/5 flex flex-col ">
+        <form onSubmit={handleSubmit} className="flex flex-col w-full  items-center">
+          <div className="flex flex-col w-full items-center gap-5 lg:gap-7 py-12">
+            <Input type="mail" label="Mail" state={email} setState={setEmail} icon={<IoMdMail />} />
+            <Input
+              type="text"
+              label="Nombre"
+              state={name}
+              setState={setName}
+              icon={<HiOutlineUser />}
+            />
+            <Input
+              type="text"
+              label="Apellido"
+              state={lastName}
+              setState={setLastName}
+              icon={<HiUser />}
+            />
+            <Input
+              type="password"
+              label="Contraseña"
+              state={password}
+              setState={setPassword}
+              icon={<AiFillEye />}
+            />
+            <Input
+              type="password"
+              label="Confirmar contraseña"
+              state={confirmPass}
+              setState={setConfirmPass}
+              icon={<AiFillEye />}
+            />
+          </div>
+          <input
+            className="mt-5 w-10/12 lg:w-2/3 py-2 rounded-full font-bold bg-gradient-to-tr from-gradone to-gradtwo"
+            type="submit"
+            value="REGISTRARSE"
           />
-          <Input
-            type='text'
-            label='Apellido'
-            state={lastName}
-            setState={setLastName}
-            icon={<HiUser />}
-          />
-          <Input
-            type='password'
-            label='Contraseña'
-            state={password}
-            setState={setPassword}
-            icon={<AiFillEye />}
-          />
-          <Input
-            type='password'
-            label='Confirmar contraseña'
-            state={confirmPass}
-            setState={setConfirmPass}
-            icon={<AiFillEye />}
-          />
-        </div>
-        <input
-          className='mt-5 w-10/12 py-2 rounded-full font-bold bg-gradient-to-tr from-gradone to-gradtwo'
-          type='submit'
-          value='REGISTRARSE'
-        />
-      </form>
+        </form>
+      </div>
     </Layout>
   );
 };
