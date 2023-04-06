4fdea3410a226c5c52f6d52f01408bf1935fb585
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-06 14:39:52 -0600

    fix: owner desktop view

--- client/src/Pages/Owner.tsx
+++ client/src/Pages/Owner.tsx
@@ -4,7 +4,7 @@ import Layout from '../Components/Layout';
 const Owner = () => {
   return (
     <Layout title="Popietarios">
-      <div className="w-full h-full overflow-scroll fixed bg-tenis bg-cover bg-[45%] pt-[50px] pb-[70px]">
+      <div className="w-full h-full overflow-scroll fixed sm:bg-tenis lg:bg-tenis-desktop bg-cover bg-[45%] pt-[50px] pb-[70px]">
         <Card route="/propietarios/canchas" title="Mis canchas" />
         <Card route="/propietarios/agregar-cancha" title="Agregar cancha" />
       </div>
