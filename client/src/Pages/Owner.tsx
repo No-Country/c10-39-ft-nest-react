efb5c7cc1406444e245e5ca175a83bf86c38fb59
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-06 15:35:41 -0600

    fix: desktop

--- client/src/Pages/Owner.tsx
+++ client/src/Pages/Owner.tsx
@@ -4,9 +4,12 @@ import Layout from '../Components/Layout';
 const Owner = () => {
   return (
     <Layout title="Popietarios">
-      <div className="w-full h-full overflow-scroll fixed sm:bg-tenis lg:bg-tenis-desktop bg-cover bg-[45%] pt-[50px] pb-[70px]">
-        <Card route="/propietarios/canchas" title="Mis canchas" />
-        <Card route="/propietarios/agregar-cancha" title="Agregar cancha" />
+      <div className="w-full h-full overflow-scroll fixed bg-tenis-desktop bg-cover bg-[30%]">
+        <div className="flex flex-col gap-16 mx-1 h-full pt-24 relative lg:flex-row lg:mx-20 lg:pt-0 lg:items-center lg:bottom-20 lg:justify-between">
+          {' '}
+          <Card route="/propietarios/canchas" title="Mis canchas" />
+          <Card route="/propietarios/agregar-cancha" title="Agregar cancha" />
+        </div>
       </div>
     </Layout>
   );
