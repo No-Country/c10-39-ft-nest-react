87e7127a783d453825d471dce36ef494be79b62b
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-06 17:57:41 -0300

    add responsive sport page

--- client/src/Pages/Reservation.tsx
+++ client/src/Pages/Reservation.tsx
@@ -11,8 +11,8 @@ const Reservation = () => {
     getAllSports(setSports).catch(() => console.log('Sports fetch failed'));
   }, []);
   return (
-    <Layout title='Deportes'>
-      <div className='w-full h-full overflow-scroll fixed bg-cover bg-[45%] pt-[50px] pb-[70px]'>
+    <Layout title="Deportes">
+      <div className="w-full h-full overflow-scroll fixed bg-cover bg-[45%]">
         {/*
         {
           sports?.map( (sport)=>{  
@@ -24,16 +24,18 @@ const Reservation = () => {
         }
          
         */}
-        <SportCard
-          bgImage={'https://www.rere.jp/beginners/uploads/2019/09/i-471621500-3-1024x667.jpg'}
-          title={'Tenis'}
-        />
-        <SportCard
-          bgImage={
-            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.TwyrhmdISweczTXM9p0RRgAAAA%26pid%3DApi&f=1&ipt=5684e1ae9b6fe7e7390755c927508d7eaf52da55cd22cd2ebc777990c8f91d9c&ipo=images'
-          }
-          title={'Futbol'}
-        />
+        <div className="flex flex-col gap-16 mx-1 h-full pt-24 relative lg:flex-row lg:mx-20 lg:pt-0 lg:items-center lg:bottom-20 lg:justify-between">
+          <SportCard
+            bgImage={'https://www.rere.jp/beginners/uploads/2019/09/i-471621500-3-1024x667.jpg'}
+            title={'Tenis'}
+          />
+          <SportCard
+            bgImage={
+              'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.TwyrhmdISweczTXM9p0RRgAAAA%26pid%3DApi&f=1&ipt=5684e1ae9b6fe7e7390755c927508d7eaf52da55cd22cd2ebc777990c8f91d9c&ipo=images'
+            }
+            title={'Futbol'}
+          />
+        </div>
       </div>
     </Layout>
   );
