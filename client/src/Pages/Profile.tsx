8fad71ae065f595c77ca787775c92c25e9e85803
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-05 21:04:28 -0300

    update rounded image, block click in image input

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -4,15 +4,15 @@ import Input from '../Components/Input';
 import { MdEdit } from 'react-icons/md';
 
 const Profile: FC = () => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState('Nayib');
+  const [lastName, setLastName] = useState('Sales');
+  const [mail, setMail] = useState('Nayib1@gmail.com');
+  const [password, setPassword] = useState('Pepe');
 
   return (
     <Layout title="Perfil">
       <div className="flex flex-col items-center w-full">
-        <div className="bg-[#000] w-32 h-32 rounded-3xl m-10"></div>
+        <div className="bg-[#000] w-36 h-36 rounded-full m-10"></div>
         <div className="w-full flex flex-col items-center gap-5">
           <Input type="text" label="Nombre" state={name} setState={setName} icon={<MdEdit />} />
           <Input
