a45c4d6e087f4fcbf20825959447bc259a58acf3
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-06 22:29:15 -0300

    add profile reservation page responsive

--- client/src/Pages/ProfileReservation.tsx
+++ client/src/Pages/ProfileReservation.tsx
@@ -8,10 +8,13 @@ const ProfileReservation: FC = () => {
 
   return (
     <Layout title="Mis reservas">
-      <div className="bg-white shadow-lg mx-2 mt-20 max-h-[500px] min-h-[500px] flex flex-col gap-2 overflow-y-scroll">
-        {Data.map((item, index) => (
-          <ReservationCard key={index} />
-        ))}
+      <div className="flex w-full justify-center">
+        {' '}
+        <div className="bg-white rounded-lg shadow-lg mx-2 mt-20 max-h-[500px] min-h-[500px] flex flex-col gap-2 overflow-y-scroll w-[700px]">
+          {Data.map((item, index) => (
+            <ReservationCard key={index} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
