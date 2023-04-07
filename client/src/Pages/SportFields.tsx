6ee708b73d3d9aee291eee48ae2e9160737494a9
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-07 16:05:19 -0300

    add some things

--- client/src/Pages/SportFields.tsx
+++ client/src/Pages/SportFields.tsx
@@ -1,16 +1,21 @@
-import { type FC } from 'react';
+import { useState, type FC, useEffect } from 'react';
 
 import Layout from '../Components/Layout';
 import SportField from '../Components/SportField';
+import { getSportFieldsWithSport } from '../Functions/SportFieldsPetition';
 
 const SportFields: FC = () => {
-  const Data = ['1', '1', '1', '1', '1', '1', '1'];
+  // const Data = ['1', '1', '1', '1', '1', '1', '1'];
+  const [Data, setData] = useState([]);
+  useEffect(() => {
+    getSportFieldsWithSport(setData, 'basketball')
+  }, []);
 
   return (
     <Layout title="Canchas">
       <div className="flex flex-row w-full justify-center gap-20 overflow-hidden max-h-[90vh]">
         <div className="scrollbarSF flex flex-col gap-5 my-5 w-full max-w-[450px] overflow-y-scroll lg:mt-10 lg:max-h-[525px] px-5">
-          {Data.map((item, index) => (
+          {Data?.map((item, index) => (
             <SportField
               key={index}
               complexData={true}
