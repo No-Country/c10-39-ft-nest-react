57db33d44b2f03e17ba715db1fbeaa39d7040158
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-06 22:22:03 -0300

    add styled scrollbar in sport fields page

--- client/src/Components/SportCard.tsx
+++ client/src/Components/SportCard.tsx
@@ -18,7 +18,7 @@ const SportCard: FC<props> = ({ children, title, bgImage }) => {
   };
   return (
     <div
-      className={`mx-1 my-[70px] relative rounded bg-opacity-10 h-[220px] w-full max-w-[700px]
+      className={`mx-1 relative rounded bg-opacity-10 h-[220px] w-full max-w-[700px] lg:mt-20
       bg-[url('https://www.rere.jp/beginners/uploads/2019/09/i-471621500-3-1024x667.jpg')] flex items-center justify-center lg:h-[350px]`}
     >
       <div className="font-segoeScript font-bold text-6xl text-center">{title}</div>
