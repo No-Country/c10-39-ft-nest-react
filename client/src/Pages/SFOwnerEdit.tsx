027cce0f2d548d2a6ec5e02600de91c7a93a23b3
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-06 23:28:26 -0300

    add title icon in owner edit field and responsive

--- client/src/Pages/SFOwnerEdit.tsx
+++ client/src/Pages/SFOwnerEdit.tsx
@@ -5,23 +5,17 @@ import Layout from '../Components/Layout';
 import PrimaryButton from '../Components/PrimaryButton';
 import { GiSoccerField } from 'react-icons/gi';
 import { GrGroup } from 'react-icons/gr';
-import { AiOutlineClockCircle } from 'react-icons/ai';
+import { MdTitle } from 'react-icons/md';
 
 const SFownerEdit: FC = () => {
   return (
     <Layout title="Editar cancha">
       <div className="relative min-h-[100vh] flex flex-col items-center">
-        <div className="bg-[#D9D9D9] rounded-lg w-10/12 cursor-pointer my-[70px] relative rounded h-[225px] lg:h-[400px] lg:w-[800px] text-center ">
+        <div className="bg-[#D9D9D9] rounded-lg w-10/12 cursor-pointer my-[70px] relative h-[225px] lg:h-[400px] lg:w-[800px] text-center ">
           +
         </div>
         <div className="flex flex-col w-full items-center gap-10 lg:w-[700px]">
-          <Input
-            type="text"
-            label="Titulo"
-            icon={<AiOutlineClockCircle />}
-            state=""
-            setState={() => {}}
-          />
+          <Input type="text" label="Titulo" icon={<MdTitle />} state="" setState={() => {}} />
           <Input
             type="text"
             label="Tipo de cancha"
@@ -32,7 +26,7 @@ const SFownerEdit: FC = () => {
           <Input type="text" label="Capacidad" icon={<GrGroup />} state="" setState={() => {}} />
         </div>
         <div className="absolute bottom-0 right-10 lg:relative lg:my-10 lg:w-[675px] lg:flex lg:justify-end">
-          <PrimaryButton text="BUSCAR" />
+          <PrimaryButton text="AGREGAR" />
         </div>
       </div>
     </Layout>
