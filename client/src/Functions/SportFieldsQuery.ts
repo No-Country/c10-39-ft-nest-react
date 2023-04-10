4b0b721c481ba9b865c3664838f90dd8f1b0e0f2
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-10 14:39:11 -0600

    fix: token authorization from login

--- client/src/Functions/SportFieldsQuery.ts
+++ client/src/Functions/SportFieldsQuery.ts
@@ -4,7 +4,7 @@ import axios from './axios';
 
 export async function getSportFieldsWithSport(sport: string, token: string) {
   try {
-    const { data }: { data: sportData[] } = await axios.get(`/sportfields/sport/${sport}`, {
+    const { data }: { data: sportData[] } = await axios.get(`/sportfields/sport/basketball`, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
