6e70492dd921b7b010ec5b9d8f4ddc446f9cf0a9
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-11 11:55:57 -0300

    create component location input

--- client/src/Components/LocationInput.tsx
+++ client/src/Components/LocationInput.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import '../types/googlemaps.d.ts'
 
 type LocationInputProps = {
   onLocationChange: (latitude: number, longitude: number) => void;
