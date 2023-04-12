b2fea3bb8f9131aca95aa6695bd64b583ff46de4
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-12 12:32:13 -0300

    add conection in edit sport field

--- client/src/Pages/SFOwnerEdit.tsx
+++ client/src/Pages/SFOwnerEdit.tsx
@@ -1,4 +1,5 @@
 import { useState, type FC, type BaseSyntheticEvent } from 'react';
+import { useSelector } from 'react-redux';
 
 import { GiSoccerField } from 'react-icons/gi';
 import { GrGroup } from 'react-icons/gr';
@@ -7,13 +8,16 @@ import { MdTitle } from 'react-icons/md';
 import Input from '../Components/Input';
 import Layout from '../Components/Layout';
 import PrimaryButton from '../Components/PrimaryButton';
+import { OwnerEditSFQuery } from '../Functions/OwnerQuery';
 
 const SFownerEdit: FC = () => {
   const [state, setState] = useState({
-    fieldType: '',
     title: '',
+    sportField: '',
     capacity: '',
   });
+
+  const userId = useSelector<any>((state) => state.user?.user?.id);
 
   const handleChange = (event: BaseSyntheticEvent) => {
     setState((prev) => {
@@ -25,9 +29,15 @@ const SFownerEdit: FC = () => {
     });
   };
 
+  const handleSubmit = (e: BaseSyntheticEvent) => {
+    e.preventDefault();
+    const token = localStorage.getItem('token') ?? '';
+    OwnerEditSFQuery(state, token, userId).catch((err) => console.log(err));
+  };
+
   return (
     <Layout title="Editar cancha">
-      <div className="relative min-h-[100vh] flex flex-col items-center">
+      <form onSubmit={handleSubmit} className="relative min-h-[100vh] flex flex-col items-center">
         <div className="bg-[#D9D9D9] rounded-lg w-10/12 cursor-pointer my-[70px] relative h-[225px] lg:h-[400px] lg:w-[800px] text-center ">
           +
         </div>
@@ -45,8 +55,8 @@ const SFownerEdit: FC = () => {
             label="Tipo de cancha"
             icon={<GiSoccerField />}
             handleChange={handleChange}
-            name="fieldType"
-            value={state.fieldType}
+            name="sportField"
+            value={state.sportField}
           />
           <Input
             type="text"
@@ -60,7 +70,7 @@ const SFownerEdit: FC = () => {
         <div className="absolute bottom-0 right-10 lg:relative lg:my-10 lg:w-[675px] lg:flex lg:justify-end">
           <PrimaryButton text="EDITAR" />
         </div>
-      </div>
+      </form>
     </Layout>
   );
 };
