3ae2a6845dabad2ad2dd040c12db1b9f4686ed53
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-04-12 01:08:02 -0300

    update user done

--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -3,6 +3,7 @@ import { PartialType } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
 
 import { RegisterUserDTO } from './register-dto';
+import { BeforeUpdate } from 'typeorm';
 
 export class UpdateUserDTO extends PartialType(RegisterUserDTO) {
   @IsEmail()
@@ -13,5 +14,9 @@ export class UpdateUserDTO extends PartialType(RegisterUserDTO) {
   password: string;
 
   @Length(3, 40)
-  fullName?: string;
+  firstName?: string;
+
+  @Length(3, 40)
+  lastName?: string;
+
 }
