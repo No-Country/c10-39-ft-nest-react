f001e183fd81c8352177b08024044bb577ab817b
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-04-11 21:26:12 -0300

    Users docs done

--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
+
 import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
 
 import { RegisterUserDTO } from './register-dto';
