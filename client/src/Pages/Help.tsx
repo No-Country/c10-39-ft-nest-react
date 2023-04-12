12d052bd319be0adf087cafaae49ecb23991e525
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-12 11:58:38 -0600

    fix: styles in help

--- client/src/Pages/Help.tsx
+++ client/src/Pages/Help.tsx
@@ -7,9 +7,9 @@ import Layout from '../Components/Layout';
 const Help: FC = () => {
   return (
     <Layout title="Ayuda">
-      <div className="flex justify-center w-full">
+      <div className="flex justify-center lg:py-12 pt-0 w-full">
         <div className="lg:w-2/5  mx-1 mt-12">
-          <div className="text-center  mb-12">En que podemos ayudarte</div>
+          <div className="text-center font-semibold mb-12">¿En que podemos ayudarte?</div>
           {questions.map((question) => {
             return <DropdownFaq key={question.id} question={question} />;
           })}
