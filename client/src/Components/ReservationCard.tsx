9cb0073b2931f40dce4bb985a2ce9c3e75594eca
gv <gonzalovasquezher@gmail.com>
2023-04-12 18:25:43 -0300

    axios changes

--- client/src/Components/ReservationCard.tsx
+++ client/src/Components/ReservationCard.tsx
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom';
 import { MdEdit } from 'react-icons/md';
 // import { BsFillShareFill } from 'react-icons/bs';
 
-const ReservationCard: FC = () => {
+interface Props {
+  title: string;
+  address: string;
+}
+
+const ReservationCard: FC<Props> = ({ title, address }) => {
   return (
-    <div className="flex flex-col w-full bg-secondary px-5 py-2">
-      <span className="opacity-70">Titulo</span>
-      <div className="flex flex-row w-full justify-between items-center">
-        <span className="text-lg">Ubicacion</span>
-        <div className="flex flex-row gap-5 text-2xl">
+    <div className='flex flex-col w-full bg-secondary px-5 py-2'>
+      <span className='opacity-70'>{title}</span>
+      <div className='flex flex-row w-full justify-between items-center'>
+        <span className='text-lg'>{address}</span>
+        <div className='flex flex-row gap-5 text-2xl'>
           <Link to={'/reservar/:sport/canchas/:id'}>
             <MdEdit />
           </Link>
