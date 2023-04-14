d06fa9c7c18e55c6494d5bf2b855c97f60a80d35
Felipe Suarez <felipesuarez1525@gmail.com>
2023-04-14 16:49:44 -0300

    update hour list component positions and scrollbar

--- client/src/Components/HoursList.tsx
+++ client/src/Components/HoursList.tsx
@@ -24,7 +24,7 @@ const HoursList: FC<{ handleClick: () => void; getAllHours: boolean }> = ({
   ]);
 
   const allHours: hoursType[] = [];
-  for (let i = 1; i <= 24; i++) {
+  for (let i = 7; i <= 24; i++) {
     const end_hour = i === 24 ? 0 : i + 1;
     const addedHour = {
       end_hour: end_hour.toString(),
@@ -47,16 +47,16 @@ const HoursList: FC<{ handleClick: () => void; getAllHours: boolean }> = ({
   return (
     <>
       {hours.length ? (
-        <div className="bg-white absolute top-32 -left-24 lg:top-0 lg:-left-full py-5 rounded-md shadow-lg">
+        <div className="bg-white absolute top-32 -left-24 lg:top-0 lg:-left-full py-5 rounded-md px-1 shadow-lg">
           <span className="mb-2 divide-black border-b-[1px] mx-2 px-1 text-center block w-[90%]">
             {getAllHours ? 'Horarios' : 'Horarios disponibles'}
           </span>
-          <ul className="overflow-y-scroll max-h-[160px] relative left-1">
+          <ul className="hoursScrollbar overflow-y-scroll max-h-[160px] relative left-1">
             {hours.map((item) => {
               return (
                 <li
                   key={item.id}
-                  className="flex cursor-pointer py-1 px-5 active:bg-primary"
+                  className="flex cursor-pointer py-1 pl-5 pr-3 active:bg-primary"
                   onClick={handleClick}
                 >
                   <span className={`w-16 ${item.start_hour.length > 1 ? 'relative right-2' : ''}`}>
