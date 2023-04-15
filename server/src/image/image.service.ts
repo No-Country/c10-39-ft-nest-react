6d6626218ace8f68639054720e9f09729faa4cf4
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-15 20:40:19 -0300

    add endpoint upload image

--- server/src/image/image.service.ts
+++ server/src/image/image.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+
+
 
 @Injectable()
 export class ImageService {
-    uploadImage(image: string) {
-        return { image };
-    }
+    async uploadImage(file: any): Promise<string> {
+        // const { path } = file;
+        const uploadedImage = await cloudinary.uploader.upload(file.buffer.toString("base64"));
+        return uploadedImage.secure_url;
+      }
 }
