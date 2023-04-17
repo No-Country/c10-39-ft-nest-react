749697969e279e67d54ec7ebba16bbb8d486b4a8
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-17 17:57:47 -0300

    add header to PostFile

--- client/src/Functions/FileQuery.ts
+++ client/src/Functions/FileQuery.ts
@@ -4,8 +4,14 @@ export async function PostFile(file: File) {
   try {
     const { data } = await axios.post<{ data: string }>('/image/upload', {
       file,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
     });
     return data;
+    
   } catch (error) {
     console.error(error);
   }
