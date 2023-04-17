a22277cd37977e07e76b34dfad38bb4c90a98478
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-17 08:04:32 -0600

    feat: turn view

--- client/src/Pages/Owner/Turns.tsx
+++ client/src/Pages/Owner/Turns.tsx
@@ -1,8 +1,19 @@
-import { FC } from 'react';
+import { type FC } from 'react';
+
 import Layout from '../../Components/layout/Layout';
+// import TurnsCard from '../../Components/TurnsCard';
 
 const Turns: FC = () => {
-  return <Layout title="Turnos"></Layout>;
+  return (
+    <Layout title="Turnos">
+      <div className="flex justify-center lg:py-12 pt-0 w-full">
+        <div className="lg:w-2/5  mx-1 mt-12">
+          <div className="text-center font-semibold mb-12">Turnos reservados</div>
+          {/* <TurnsCard />; */}
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Turns;
