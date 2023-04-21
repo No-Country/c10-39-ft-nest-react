2947786672c41eb63fd7cb316745928d08eb4f06
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-04-20 19:59:52 -0600

    fix: loader reservations

--- client/src/Pages/Sports/Reservation.tsx
+++ client/src/Pages/Sports/Reservation.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 
 import SportCard from '../../Components/cards/SportCard';
 import Layout from '../../Components/layout/Layout';
-import { getAllSports } from '../../Functions/SportQuery';
 import Loader from '../../Components/Loader/Loader';
+import { getAllSports } from '../../Functions/SportQuery';
 
 interface SportItem {
   id: string;
@@ -13,47 +13,40 @@ interface SportItem {
 
 const Reservation = () => {
   const [sports, setSports] = useState<SportItem[] | []>([]);
-  const [loader, setLoader] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(false);
+
     getAllSports()
-      .then((data) => data && setSports(data))
+      .then((data) => {
+        data && setSports(data);
+        setLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
-  setTimeout(() => {
-    setLoader(false);
-  }, 1000);
-
   return (
-    <>
-      {loader ? (
-        <Loader></Loader>
-      ) : (
-        <Layout title="Deportes">
-          <div className="w-full h-full overflow-scroll bg-cover bg-[45%] py-5">
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-2 lg:px-10 lg:py-5">
-              {sports.length ? (
-                sports.map((sport) => {
-                  return (
-                    <SportCard
-                      key={sport.id}
-                      href={`/reservar/${sport.name}`}
-                      bgImage={sport.images[0]}
-                      title={sport.name}
-                    />
-                  );
-                })
-              ) : (
-                <h2 className="text-2xl text-center flex w-screen mt-20 justify-center items-center">
-                  ERROR - DEPORTES EN MANTENIMIENTO
-                </h2>
-              )}
-            </div>
-          </div>
-        </Layout>
-      )}
-    </>
+    <Layout title="Deportes">
+      <div className="w-full h-full overflow-scroll bg-cover bg-[45%] py-5">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-2 lg:px-10 lg:py-5">
+          {loading ? (
+            sports.map((sport) => {
+              return (
+                <SportCard
+                  key={sport.id}
+                  href={`/reservar/${sport.name}`}
+                  bgImage={sport.images[0]}
+                  title={sport.name}
+                />
+              );
+            })
+          ) : (
+            <Loader></Loader>
+          )}
+        </div>
+      </div>
+    </Layout>
   );
 };
 
